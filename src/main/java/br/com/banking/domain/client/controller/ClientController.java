ad2fb629df857e5dd6981a316d137069a551a151
Alyson Vieira <alysonvieira88@gmail.com>
2024-05-23 20:14:45 -0300

    Ajustes do swagger

--- src/main/java/br/com/banking/domain/client/controller/ClientController.java
+++ src/main/java/br/com/banking/domain/client/controller/ClientController.java
@@ -3,7 +3,8 @@ package br.com.banking.domain.client.controller;
 import br.com.banking.domain.client.Client;
 import br.com.banking.domain.client.request.ClientRequest;
 import br.com.banking.domain.client.service.ClientService;
-import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,16 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(name = "/clients")
-@ApiModel(description = "")
+@RequestMapping("/clients")
+@Api(value = "Client Management", description = "Operations pertaining to client management")
 public class ClientController {
 
     @Autowired
     private ClientService clientService;
 
+    @ApiOperation(value = "Create a new client")
     @PostMapping("/create")
     public ResponseEntity<Client> post(@RequestBody ClientRequest request) {
         return clientService.criaClienteSeNaoExistir(request);
     }
-
 }
